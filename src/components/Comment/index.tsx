225434a6d603b682c71b831e8cdffd28beba4068
Filipe Lucena <filipelucena1@gmail.com>
2023-04-14 17:35:31 -0300

    feat: mocked CarDetailPage

---
+++ src/components/Comment/index.tsx
@@ -0,0 +1,39 @@
+import { Flex } from "@chakra-ui/layout";
+import { Heading, VStack, Text, Avatar } from '@chakra-ui/react'
+
+const Comment = ({userName, comment, time, color}: {userName: string, comment: string, time: string, color: string}) => {
+	return (
+		<>
+			<VStack
+				alignItems="start"
+			>
+				<Flex
+					alignItems="center"
+				>
+					<Avatar
+						mr="10px"
+						name={userName}
+						size="sm"
+						backgroundColor={color}
+					></Avatar>
+					<Heading mr="15px" as="h3" size="7" variant="500">
+						Samuel Leão
+					</Heading>
+					<Text
+						color="gray.2"
+					>
+						{time}
+					</Text>
+				</Flex>
+				<Text
+					textAlign="justify"
+					size="8"
+				>
+					{comment}
+				</Text>
+			</VStack>
+		</>
+	);
+};
+
+export default Comment;
